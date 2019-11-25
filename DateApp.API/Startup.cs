a1a2da235487204b11e5e3fc49c756378c3eedb3
vladimirpetukhov <vlado_web@abv.bg>
2019-11-25 07:13:39 +0200

    ADD:ErorrHandlerMiddleware

--- DateApp.API/Startup.cs
+++ DateApp.API/Startup.cs
@@ -1,57 +1,62 @@
-namespace DateApp.API
-{
+namespace DateApp.API {
+    
     using Data;
+    using Helpers;
+    using System.Net;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Diagnostics;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(x =>
-           x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddControllers();
-            services.AddCors();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (x =>
+                x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
+            services.AddScoped<IAuthRepository, AuthRepository> ();
+            services.AddControllers ();
+            services.AddCors ();
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme);
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_3_0);
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler (builder => {
+                    builder.Run (async context => {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature> ();
+                        if(error!=null){
+                            context.Response.AddAppError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             //app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting ();
 
             //app.UseAuthorization();
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseCors (x => x.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
 
         }
